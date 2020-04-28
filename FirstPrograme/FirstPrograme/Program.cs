4df8e0b3c536241ab371a6e5211b2a7c936052bf
Fadi-Elkortachi <64487506+Fadi-Elkortachi@users.noreply.github.com>
2020-04-28 17:23:50 +0000

    First C# Programe
    My First C# Programe

---
+++ FirstPrograme/FirstPrograme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPrograme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This My First Programe in CSharp
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+
+        }
+    }
+}
