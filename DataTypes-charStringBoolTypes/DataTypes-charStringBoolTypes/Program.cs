41d543c424a9a512acfc58202aee37e1cecd7b18
Fadi-Elkortachi <64487506+Fadi-Elkortachi@users.noreply.github.com>
2020-04-29 01:39:26 +0000

    DataTypes-CharStringBooleanTypes
    This is a Programe To Explain These Types in CS

---
+++ DataTypes-charStringBoolTypes/DataTypes-charStringBoolTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_charStringBoolTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int charMin = Char.MinValue;
+            int charMax = Char.MaxValue;
+
+            char MyVar = 'A';
+            int myVarValue = 'A';
+
+            char myVarVlue2 = Convert.ToChar(myVarValue);
+
+            Console.WriteLine("Min char = {0} Max char = {1} My character = {2} my Variable Number = {3} my Conversion Value = {4}" , charMin, charMax, MyVar, myVarValue, myVarVlue2);
+
+            string myString = "This is My Variable";
+            Console.WriteLine("\nMy String is {0} ", myString);
+
+            bool myBool = true;
+            Console.WriteLine("my Boolean Value = {0}", myBool);
+          
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Test n1/Test n1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_n1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char myVar = 'A';
+            int myVarValue = 'A';
+
+            char myVar2 = 'a';
+            int myVarValue2 = 'a';
+
+
+
+            Console.WriteLine(" This is my Chaaracter A value = {0} and This is My Character a Value = {1}", myVarValue, myVarValue2);
+
+          //string myString = "65";
+          
+           //Console.WriteLine("\nThis is My Character A Value {0}", myString);
+
+           // string myString2 = "97";
+           // Console.WriteLine("\nThis is My Character a Value {0}", myString2);
+            Console.ReadKey();
+
+        }
+    }
+}
